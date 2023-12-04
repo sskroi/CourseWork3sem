e8bc68267f70e553ba94e9d3595b53a461bd1022
Andrei E <matteoup@ya.ru>
2023-12-05 01:51:40 +0300

    changed error text in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -52,19 +52,19 @@ void initDatabase(RouteList& list) {
     RouteList fromDb;
     try {
         if (!fromDb.readFromFile(DB_FILENAME)) {
-            std::cout << "\x1B[38;5;197mФайл с базой данных - \"\x1B[38;5;122m" << DB_FILENAME << "\x1B[38;5;197m\"\n";
+            std::cout << "\x1B[38;5;197mФайл с базой данных \"\x1B[38;5;122m" << DB_FILENAME << "\x1B[38;5;197m\"\n";
             std::cout << "НЕ обнаружен в директории программы\x1B[0m\n";
             std::cout << "Будет создан \x1B[38;5;105mпустой\x1B[0m файл с БД\n\n";
-            std::cout << "Нажмите Enter чтобы перейти в главное меню . . . ";
+            std::cout << "Нажмите \x1B[38;5;216mEnter\x1B[0m чтобы перейти в главное меню . . . ";
             menu::waitEnter();
             fromDb.writeInFile(DB_FILENAME);
         }
         list = fromDb;
     } catch (const std::exception& ex) {
-        std::cout << "\x1B[38;5;197mФайл с базой данных - \"\x1B[38;5;122m" << DB_FILENAME << "\x1B[38;5;197m\" повреждён.\x1B[0m\n";
+        std::cout << "\x1B[38;5;197mФайл с базой данных \"\x1B[38;5;122m" << DB_FILENAME << "\x1B[38;5;197m\" повреждён.\x1B[0m\n";
         std::cout << "Ошибка: " << ex.what() << std::endl;
         std::cout << "Он будет перезаписан \x1B[38;5;105mновым пустым\x1B[0m файлом\n";
-        std::cout << "Нажмите Enter чтобы перейти в главное меню . . . ";
+        std::cout << "Нажмите \x1B[38;5;216mEnter\x1B[0m чтобы перейти в главное меню . . . ";
         menu::waitEnter();
         list.clear();
         list.writeInFile(DB_FILENAME);
